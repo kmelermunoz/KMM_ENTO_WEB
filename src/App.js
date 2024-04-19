dbf4e8f365599c188efcfab0fb5fc1a86dad00d4
Kevin <kevinmelerlsgi@gmail.com>
2024-04-19 10:53:29 +0200

    canvis

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test/test.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Test/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. KEVIN
         </p>
         <a
           className="App-link"
---
+++ src/Test/test.js
@@ -0,0 +1,9 @@
+function Test () {
+    return(
+    <div className="">
+        Soc un test
+    </div>
+    );
+}
+
+export default Test;
